5c1f2f80e06311440d70755d26b4e976df12aa80
UncleBogdan228 <oduch200608@gmail.com>
2025-02-05 16:54:13 +0000

    лекция

---
+++ task1_1.py
@@ -0,0 +1,42 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+frames = 200
+t = np.linspace(0,5,frames)
+g = 9.8
+v = 20
+alpha = 60 * np.pi / 180
+x0 = 0
+vx0 = v * np.cos(alpha)
+y0 = 0
+vy0 = v * np.sin(alpha)
+my = 0.2
+
+z0 = x0, vx0, y0, vy0
+
+def move_func(z,t):
+    x, vx, y, vy = z
+    dx_dt = vx
+    dvx_dt = - my * vx ** 2
+
+    dy_dt = vy
+    dvy_dt = - g 
+    return dx_dt, dvx_dt, dy_dt, dvy_dt
+
+sol = odeint(move_func, z0, t)
+fig,ax = plt.subplots()
+ball, = plt.plot([],[], 'o', color='r')
+ball_line, = plt.plot([],[], '-', color = 'r')
+
+def animate(i):
+    ball.set_data([sol[i][0]], [sol[i][2]])
+    ball_line.set_data(sol[:i, 0], sol[:i, 2])
+
+ani = FuncAnimation(fig, animate, frames= frames, interval = 30)
+
+edge = 30
+ax.set_xlim(0, edge)
+ax.set_ylim(0, edge)
+ani.save('task1_1.gif', writer = "pillow")
